93c91f5aa49f6ae7e2117f7ea1772a34996121b8
sumtwelve <sumtwelve@gmail.com>
2023-01-23 18:29:32 -0700

    reverted to functions calling each other, simpler that way. added temperature gradient image for an idea i might implement later

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -1,32 +1,45 @@
 // Grab HTML elements
 var searchInputBar = $("#city-search-bar");
 var cityHeader = $("#city-header");
-var dateHeader = $("#date-header");
+var currentDateHeader = $("#current-date-header");
+var forecastDateHeader = $("#forecast-date-header");
 var dayCardsContainer = $("#day-cards-container");
 
 
 
-// Main body of script
+// Function Declarations Below
 
+
+// Function takes query parameter from the URL bar at the top of the browser.
+// There should only ever be one parameter, because only one is ever assigned,
+// and assignation is the only way a user will normally get to this page.
 function getCityNameFromSearchBar() {
     var searchParam = document.location.search;
     if (searchParam) { // if search parameter is NOT empty or null
         var cityNameQuery = searchParam.substring(3);
+        return cityNameQuery;
     } else {
-        console.log("Error: no search query. Normally this is impossible.")
-    }
-    // uncomment ↓ to print headers to console
-    //console.log(cityNameQuery);
-    return cityNameQuery;
+        // If user manages to get to search-results.html without a `?q=` in the URL bar,
+        // then redirect to landing page to start the search process properly.
+        console.log("Error: no search query in the URL bar.")
+        cityHeader.text("Error: No search query detected. Redirecting to landing page...");
+        setTimeout(function() {
+            document.location.assign("./index.html");
+        }, 3000);
+        return;
+    }    
 }
 
-function getCoords(cityName) {
-    // To get weather data by city name from OpenWeatherMap, we need
-    // the coordinates of the city. We'll make one request for the coords,
-    // and then use those coords to request the weather data.
-    var coordsQueryUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=af97bd5be6a8a1d925cf64d77d34d415";
 
-    fetch(coordsQueryUrl)
+
+// API call to OpenWeather's Geocoding API.
+// Take a city name and return the coordinates. This is necessary because the API calls
+// to get weather data take coordinates as input, not city name.
+function getCoords(cityName) {
+
+    var coordsQueryString = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=af97bd5be6a8a1d925cf64d77d34d415";
+
+    fetch(coordsQueryString)
     .then(function (response) {
         if (!response.ok) {
             throw response.json();
@@ -38,68 +51,108 @@ function getCoords(cityName) {
         if (!results.length) { // if results has no length then search found nothing
             console.log("No results found!")
             cityHeader.text("No results found for \"" + cityName + "\"")
+            return null;
         } else {
-            console.log("City found: " + results[0].name); // print it just so I can see the headers in the console
-            console.log(results);
+            console.log("City found: " + results[0].name);
+            console.log(results); // print it just so I can see the headers in the console
             console.log("Lat: " + results[0].lat + "\nLon: " + results[0].lon);
-            var lattitude = results[0].lat;
+            var latitude = results[0].lat;
             var longitude = results[0].lon;
-            return [lattitude, longitude];
+
+            // place city and country into cityHeader
+            // This is done here because the 
+            cityHeader.text(results[0].name + ", " + results[0].country);
+
+            getCurrentWeather(latitude, longitude);
+            getForecast(latitude, longitude);
         }
     });
 }
 
-function getWeather(latLonArr) {
-    var lat = latLonArr[0];
-    var lon = latLonArr[1];
 
-}
+// Useful API links:
+// Weather icons: https://openweathermap.org/weather-conditions
 
-function getForecast(latLonArr) {
-    var cityLat = latLonArr[0];
-    var cityLon = latLonArr[1];
+
+// Make API call to OpenWeather's "Current Weather Data" API, then display the data.
+// Note that this is not a forecast. It is the current weather conditions for the area.
+// API: https://openweathermap.org/current
+function getCurrentWeather(lat, lon) {
+
+    var weatherQueryString = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=af97bd5be6a8a1d925cf64d77d34d415"
     
-    var weatherQueryString = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=af97bd5be6a8a1d925cf64d77d34d415";
-
     fetch(weatherQueryString)
     .then(function (response) {
         if (!response.ok) {
             throw response.json();
         }
-
         return response.json();
     })
     .then(function (results) {
         if (results.length === 0) {
-            console.log("No weather data results found for given coordinates.");
-            dayCardsContainer.text("Error: weather data not found for given city's coordinates.");
+            console.log("No weather data found for \'" + lat + ", " + lon + "\'");
+            dateHeader.text("Error: No weather data avilable, or failed to retrieve it.")
         } else {
-            console.log("Weather data results:");
+            console.log("Weather data results:")
             console.log(results);
-
-            return results;
+            displayCurrentWeather(results);
         }
     });
 }
 
-
-function displayWeatherData(data) {
-    // place city name at top of weather section
-    var cityName = data.city.name;
-    var cityCountry = data.city.country;
-    cityHeader.text(cityName + ", " + cityCountry);
+function displayCurrentWeather(data) {
+    // NOTE: 
     
     // display forecast date range
     var currentDate = dayjs().format("MMM D, YYYY");
-    var fiveDaysFromNow = dayjs().add(4, "day").format("MMM D, YYYY");
-    dateHeader.text(currentDate + " — " + fiveDaysFromNow);
+    currentDateHeader.text(currentDate);
+    
 
     // Find and set the content for the day cards
-    console.log(dayCardsContainer);
+    
+
+}
+
+
+
+
+// Make API call to OpenWeather's "5 Day / 3 Hour Forecast" API. Does not display data.
+// Note that this is a forecast, so today's weather will not be present in the data.
+// API: https://openweathermap.org/forecast5
+function getForecast(lat, lon) {
+
+    var forecastQueryString = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=af97bd5be6a8a1d925cf64d77d34d415";
+    
+    fetch(forecastQueryString)
+    .then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
+    .then(function (results) {
+        if (results.length === 0) {
+            console.log("No forecast data found for \'" + lat + ", " + lon + "\'");
+            dayCardsContainer.text("Error: No forecast data avilable, or failed to retrieve it.");
+        } else {
+            console.log("Forecast data results:");
+            console.log(results);
+            displayForecast(results);
+        }
+    });
+}
+
+// Display the data retrieved by getForecast().
+function displayForecast(data) {
+
+    var currentDate = dayjs().format("MMM D, YYYY");
+    var fiveDaysFromNow = dayjs().add(4, "day").format("MMM D, YYYY");
+    forecastDateHeader.text(currentDate + " — " + fiveDaysFromNow);
+
     for (var i = 0; i < 5; i++) {
         // Set day text (e.g. "Sun") and date text (e.g. "1/23")
-        var day = dayjs().add(i, "day").format("ddd");
-        var date = dayjs().add(i, "day").format("M/D")
+        var day = dayjs().add(i+1, "day").format("ddd");
+        var date = dayjs().add(i+1, "day").format("M/D")
         dayCardsContainer.children().eq(i).children().eq(0).text(day);
         dayCardsContainer.children().eq(i).children().eq(1).text(date);
 
@@ -111,9 +164,14 @@ function displayWeatherData(data) {
     }
 }
 
+
+
+
 function addToSearchHistory(query) {
-    // TODO: display previous searches as an <a> tag
+    // TODO: display previous searches as <a> tags
 }
+
+
 
 
 // Provide functionality to search bar. Exact same as on landing page.
@@ -129,7 +187,12 @@ $("#search-form").on("submit", function(event) {
     }
 });
 
+
+
+
+
+// Start the program.
 var cityName = getCityNameFromSearchBar();
 var cityCoords = getCoords(cityName); // API call to find coordinates by city name
-var currentWeatherData = getWeather(cityCoords); // API call to get current weather data
-var forecastData = getForecast(cityCoords); // API call to get 5-day forecast starting tomorrow
+// once it has the coords, it will call getCurrentWeather() to display current weather data,
+// and then getCurrentWeather() will call getForecast() to display forecast data
